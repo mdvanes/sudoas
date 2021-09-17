1826b5e0b87b5f5e1dc14d5d1b8a3d824ff948f5
mdvanes <es@mdworld.nl>
2021-09-17 11:19:24 +0200

    Add HTML

--- tests/index.ts
+++ tests/index.ts
@@ -5,7 +5,7 @@ import { performance } from "perf_hooks";
 import myModule from "..";
 
 const { __pin, getPrimes } = myModule;
-// Fix ts-expect-error with exporting ID?
+// Fix ts-expect-error with exporting ID? TODO
 // @ts-expect-error
 const { __getArray } = myModule;
 const NR_OF_PRIMES = Math.pow(10, 3);
@@ -13,7 +13,6 @@ const NR_OF_PRIMES = Math.pow(10, 3);
 try {
     const before = performance.now();
     const primes = __getArray(__pin(getPrimes(NR_OF_PRIMES)));
-    // ...performance.now...
     const after = performance.now();
     const lastPrime = primes[primes.length - 1]; 
     console.log(`last prime: ${lastPrime}, time taken: ${after - before}`);
@@ -22,6 +21,6 @@ try {
     console.log(err);
 }
 
-//...js reference call...
+//...js reference call... TODO
 
 console.log("ok");
