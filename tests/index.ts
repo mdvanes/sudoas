a0e5280b7d769f587cc131bd34df9f3a92669668
mdvanes <es@mdworld.nl>
2021-09-17 11:10:06 +0200

    Fix call

--- tests/index.ts
+++ tests/index.ts
@@ -3,16 +3,23 @@ import assert from "assert";
 // @ts-ignore
 import { performance } from "perf_hooks";
 import myModule from "..";
+
+const { __pin, getPrimes } = myModule;
 // Fix ts-expect-error with exporting ID?
 // @ts-expect-error
 const { __getArray } = myModule;
 const NR_OF_PRIMES = Math.pow(10, 3);
 
 try {
-    const primes = __pin(getPrimes(NR_OF_PRIMES));
+    const before = performance.now();
+    const primes = __getArray(__pin(getPrimes(NR_OF_PRIMES)));
     // ...performance.now...
+    const after = performance.now();
+    const lastPrime = primes[primes.length - 1]; 
+    console.log(`last prime: ${lastPrime}, time taken: ${after - before}`);
+    assert.strictEqual(lastPrime, 997, "highest prime in list");
 } catch (err) {
-    // ...
+    console.log(err);
 }
 
 //...js reference call...
